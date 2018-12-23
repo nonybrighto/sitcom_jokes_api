6807b3a50be0b34a2a4056d2de81ba5ec54c69ca
nonybright <nonybrighto@gmail.com>
2018-12-23 17:55:39 +0100

    implement pagination

---
+++ app/helpers/pagination.js
@@ -0,0 +1,53 @@
+class Pagination{
+
+
+        constructor(pageUrl, totalCount, page, perPage){
+            this.page = page;
+            this.perPage = perPage;
+            this.pageUrl = pageUrl;
+            this.totalCount = totalCount;
+        }
+
+        getOffset(){
+            let offset = (this.page - 1) * this.perPage;
+            return offset;
+        }
+
+        getTotalPages(){
+            let totalPages = Math.ceil(this.totalCount / this.perPage);
+            return totalPages;
+        }
+
+        hasNextPage(){
+                return this.page < this.getTotalPages();
+        }
+
+        hasPreviousPage(){
+                return this.page > 1;
+        }
+
+        generateLinkHeader(){
+            
+
+
+        }
+
+        generatePaginationObject(){
+
+            let paginationObject = {
+                totalPages: this.getTotalPages(),
+                perPage: this.perPage,
+                currentPage: this.page,
+                ...(this.hasPreviousPage() && {previousPage: --this.page}),
+                ...(this.hasNextPage() && {nextPage: ++this.page}),
+
+            };
+
+            return paginationObject;
+        }
+
+
+}
+
+
+module.exports = Pagination;
