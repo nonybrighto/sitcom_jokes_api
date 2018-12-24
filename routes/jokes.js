cf62a04f389c962d2bea3d2a8c4c68a378615780
nonybright <nonybrighto@gmail.com>
2018-12-24 19:22:30 +0100

    add pagination midleware to get jokes route

--- routes/jokes.js
+++ routes/jokes.js
@@ -6,12 +6,18 @@ const commentValidator = require('../app/middlewares/validators/comment_validato
 const JokeController = require('../app/controllers/joke_controller');
 const authMiddleWare = require('../app/middlewares/auth_middleware');
 const paginationMiddleWare = require('../app/middlewares/pagination_middleware');
+const FileUploader = require('../app/helpers/file_uploader');
+const multer = require('multer');
 
+
+const upload = multer();
+
+//const fileUploader = new FileUploader();
 
 
 router.route('/')
-      .get(JokeController.getJokes)                 
-      .post([authMiddleWare.isJwtAuthenticated, validate(jokeValidator.createJoke)], JokeController.addJoke);
+      .get([paginationMiddleWare], JokeController.getJokes)                 
+      .post([authMiddleWare.isJwtAuthenticated, validate(jokeValidator.createJoke), upload.single('image')], JokeController.addJoke);
 
 router.route('/:jokeId')
       .get(JokeController.getJoke);
