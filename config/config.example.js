d1e6cf36b341ddea73a297aa57ba74d9e512bc89
nonybright <nonybrighto@gmail.com>
2019-04-06 20:53:13 +0100

    remove uneeded config

--- config/config.example.js
+++ config/config.example.js
@@ -3,28 +3,25 @@
 var nconf = require('nconf');
 
 nconf.defaults({
-	'neo4j-username': 'username',
-	'neo4j-password' : 'password',
+	'api-v1-url': 'http://localhost:3000/api/v1/',
+	'neo4j-username': '',
+	'neo4j-password' : '',
 	'env': 'development',
 	'neo4j': 'local',
 	'neo4j-local': 'bolt://localhost',
 	'neo4j-remote': 'bolt:http://162.243.100.222:7687',
-	'base_url': 'http://localhost:3000',
-	'api_path': '/api/v0',
+	'base-url': 'http://localhost:3000/',
 	'gmail-email':'nonybrighto@gmail.com',
 	'gmail-password':'will_be_added_when_needed',
 	'email-host':'email-server host',
 	'email-port':587,
 	'email-user':'user',
 	'email-password':'password',
-	'jwt-secret': '0a6a257d-d3fe-45fc-a35e-0295c986cd9f',
-	'uuid-namespace': '1b514e64-41d5-591e-54b0-cd01fe1f3341',
-	'auth0-domain':'nonyapp.auth0.com',
-	'auth0-api-audience':'http://localhost:3000/api/v1/',
-	'facebook-client-id':'cleint_id',
-	'facebook-client-secret':'secret',
-	'google-client-id':'id',
-	'google-client-secret':'',
+	'jwt-secret': '0a6b357d-d2fb-46fc-a84e-0295a986cd9f',
+	'uuid-namespace': '1b512364-41d5-456e-54b0-cda1fe1f3341',
+	'facebook-client-id':'',
+	'facebook-client-secret':'',
+	'google-client-id':'',
 	'password-token-expire-hrs':2
 
 });
