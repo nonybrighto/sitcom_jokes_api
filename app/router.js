100d49af405d67aa6a591fd66460f3c339140c98
nonybright <nonybrighto@gmail.com>
2019-04-09 14:38:21 +0100

    create authenticated user route

--- app/router.js
+++ app/router.js
@@ -3,15 +3,16 @@ var router = function(app){
     const v1 = '/api/v1';
   
     const users = require('../routes/users');
+    const user = require('../routes/user');
     const jokes = require('../routes/jokes');
     const auth = require('../routes/auth');
     const movies = require('../routes/movies');
     //   var auth = require('../routes/auth.route');
     //   var countries = require('../routes/country.route');
    
-      //TODO: remember to add /api/v1 when testing the rest API
       app.use(v1+'/auth',auth);
       app.use(v1+'/users', users);
+      app.use(v1+'/user', user);
       app.use(v1+'/jokes', jokes);
       app.use(v1+'/movies', movies);
   };
---
+++ routes/user.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+var UserController = require('../app/controllers/user_controller');
+const authMiddleWare = require('../app/middlewares/auth_middleware');
+const paginationMiddleWare = require('../app/middlewares/pagination_middleware');
+
+
+router.route('/favorites/jokes')
+      .get([authMiddleWare.jwtAuthentication, paginationMiddleWare], UserController.getUserFavoriteJokes)
+
+router.route('/liked/joke/:jokeId')
+      .put([authMiddleWare.jwtOptionalAuthentication],UserController.likeJoke)
+      .delete([authMiddleWare.jwtAuthentication],UserController.unlikeJoke)
+
+router.route('/favorited/joke/:jokeId')
+      .put([authMiddleWare.jwtAuthentication], UserController.addJokeToFavorite)
+      .delete([authMiddleWare.jwtAuthentication], UserController.removeJokeFromFavorite)
+
+
+
+module.exports = router;
