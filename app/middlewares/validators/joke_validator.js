487752952a48d7f4250648a20fb75601c123732e
nonybright <nonybrighto@gmail.com>
2019-05-02 04:41:41 +0100

    reduce joke characters number

--- app/middlewares/validators/joke_validator.js
+++ app/middlewares/validators/joke_validator.js
@@ -5,7 +5,7 @@ const createJokeSchema = {
     body: {
         title: Joi.string().trim().min(3).required().label('Title should be 3 characters or more'),
         tmdbMovieId: Joi.number().integer().required().label('Please provide a movie'),
-        text: Joi.string().trim().min(30).label('text should be more than 30 characters')
+        text: Joi.string().trim().min(20).label('text should be more than 20 characters')
     }
 }
 
